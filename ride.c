380beedc75bf8c6d98f33f0e7efeeb205242fc1a
iodo <elissa.y.he@gmail.com>
2014-12-14 17:35:23 -0500

    ride done

--- ride.c
+++ ride.c
@@ -10,27 +10,27 @@ int num(char * name)
 {
     int prod = 1;
     int i = 0;
-    while (i < 10) {
-        int num = name[i] - 'a' + 1;
-        prod = prod * num;
+    while (name[i] != '\0') {
+        prod *= name[i] - 64;
         i++;
     }
     return prod % 47;
 }
 
-int main() {
+int main()
+{
     FILE *fin = fopen ("ride.in", "r");
-    FILE *fout = fopen ("ride.out", "r");
+    FILE *fout = fopen ("ride.out", "w");
 
-    char comet[10], group [10];
-    fgets(fin, 10, comet);
-    fgets(fin, 10, group);
+    char comet[10], group[10];
+
+    fscanf(fin, "%s", comet);
+    fscanf(fin, "%s", group);
 
     if (num(comet) == num(group))
         fprintf(fout, "GO\n");
     else
         fprintf(fout, "STAY\n");
 
-    return(0);
+    return 0;
 }
-
